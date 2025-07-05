1604cd15762452b5840070f0aef355d7dbc1ecc1
Yash Dnyaneshwar Shelke <yashshelke885@gmail.com>
2025-07-05 11:54:17 +0530

    competed 1st exercise of week 3

---
+++ Week_3/Week_3_Spring Core_Maven/4_Configuring_Maven_Project/code/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+
+public class BookRepository {
+
+    public void saveBook(String bookName) {
+        System.out.println("BookRepository: " + bookName);
+    }
+}
+
---
+++ Week_3/Week_3_Spring Core_Maven/4_Configuring_Maven_Project/code/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName) {
+        System.out.println("BookService: " + bookName);
+        bookRepository.saveBook(bookName);
+    }
+}
+
